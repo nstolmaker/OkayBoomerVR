171db5d49069f4bd676d5adb9204833b7d7650c0
Noah Stolmaker <nstolmaker@gmail.com>
2020-05-03 13:56:03 -0700

    incremental commit. this is broken right now actually

---
+++ Valem Jam Project 2020/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    /* there will be lots of sound effects and sound related stuff in this project. 
+     * This seems like a good place to put some of that. My unity architecture isn't strong though
+     * so if there's a preferred way, tell me! (Noah)
+     * Also having a single game object that can manage audio state is useful since audio is tricky
+     * */
+}
