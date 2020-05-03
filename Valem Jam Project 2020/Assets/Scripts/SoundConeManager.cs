5612f0cc9abe060fdfcdccb02de02eade1763a68
Noah Stolmaker <nstolmaker@gmail.com>
2020-05-03 11:16:07 -0700

    changing colliders and the OnTriggerEnter on the mic so that its based on the mic head and not just anywhere on the stand

---
+++ Valem Jam Project 2020/Assets/Scripts/SoundConeManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundConeManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject collidingWith;
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        collidingWith = collider.gameObject;
+        Debug.Log("colliding with " + collider.name);
+    }
+}
