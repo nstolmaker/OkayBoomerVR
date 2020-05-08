39583b62683cc17e2d56a6f55e8947846df06d67
Nathan Rohner <nrjedi@live.com>
2020-05-08 17:13:36 +1000

    Moved Assets to be more organized.
    Tried to be a little more organized.

---
+++ Valem Jam Project 2020/Assets/Scripts/MenuInteraction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.XR;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if LIH_PRESENT
+using UnityEngine.Experimental.XR.Interaction;
+#endif
+
+public class MenuInteraction : MonoBehaviour
+{
+    private void Start()
+    {
+        //interactor = controller.GetComponent<XRRayInteractor>();
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitToDesktop()
+    {
+        Application.Quit();
+    }
+}
