bf489713ca47944a4bce0bb6d19ee6627e3d2dec
CoconutDawg <64672429+CoconutDawg@users.noreply.github.com>
2020-05-09 02:01:14 +1000

    Final push and testing

---
+++ Valem Jam Project 2020/Assets/Scripts/ThankYouSceneSend.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ThankYouSceneSend : MonoBehaviour
+{
+    void Awake()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+
+    void Start()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
